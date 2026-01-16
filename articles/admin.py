e1e13e6f69001e374523855bbf60d07ac8c18ac3
shoudle <shoudle@shizhongyue98@yeah.net>
2026-01-16 16:04:54 +0800

    1116

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
+
+
+# 多行结构
+# class CommentInline(admin.StackedInline):
+#     model = Comment
+#     extra = 0
+
+
+# 单行结构
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [CommentInline]
+
 
 # Register your models here.
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
--- articles/views.py
+++ articles/views.py
@@ -5,10 +5,14 @@ from django.views.generic import (
     UpdateView,
     DetailView,
     CreateView,
+    View,
+    FormView,
 )
-from django.urls import reverse_lazy
+from django.views.generic.detail import SingleObjectMixin
+from django.urls import reverse_lazy, reverse
 from django.template.loader import select_template
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .forms import CommentForm
 
 
 # Create your views here.
@@ -44,9 +48,44 @@ class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         return self.get_object().author == self.request.user
 
 
-class ArticleDetailView(LoginRequiredMixin, DetailView):
+class CommentGet(DetailView):
     model = Article
     template_name = "articles/article_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        return context
+
+
+class CommentPost(FormView, SingleObjectMixin):
+    model = Article
+    form_class = CommentForm
+    template_name = "article_detail.html"
+
+    def post(self, request, *args, **kwargs):
+        self.object1 = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.article = self.object1
+        comment.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        article = self.object1
+        return reverse("article_detail", kwargs={"pk": article.pk})
+
+
+class ArticleDetailView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
 
 
 class ArticleCreateView(LoginRequiredMixin, CreateView):
